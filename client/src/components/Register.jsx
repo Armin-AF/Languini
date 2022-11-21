435dff590d864844952645994b20e2193d656297
dtimmins87 <59541874+dtimmins87@users.noreply.github.com>
2022-11-21 22:44:42 +0100

    Refactor register page

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -26,24 +26,12 @@ export const Register = (props) => {
           className="max-w-[400px] w-full mx-auto bg-white p-4 rounded-3xl"
           onSubmit={handleSubmit}
         >
-          <h2 className="text-4xl font-bold text-center py-6">App Name</h2>
+          <h2 className="text-4xl font-semibold text-center py-6">App Name</h2>
+          <h2 className="text-3xl font-semibold text-center py-6">Create Account</h2>
 
           <div className="flex flex-col py-2">
-            <label className="font-semibold">Full Name</label>
             <input
               className="border p-2 rounded-2xl"
-              type="text"
-              placeholder="Email"
-              id="email"
-              name="email"
-            />
-          </div>
-          <div className="flex flex-col py-2">
-            <label className="font-semibold">Email</label>
-            <input
-              className="border p-2 rounded-2xl"
-              value={name}
-              onChange={(e) => setEmail(e.target.value)}
               type="text"
               placeholder="Full Name"
               id="name"
@@ -51,13 +39,34 @@ export const Register = (props) => {
             />
           </div>
           <div className="flex flex-col py-2">
-            <label className="font-semibold">Password</label>
+            <input
+              className="border p-2 rounded-2xl"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="text"
+              placeholder="Email"
+              id="email"
+              name="email"
+            />
+          </div>
+          <div className="flex flex-col py-2">
             <input
               className="border p-2 rounded-2xl"
               value={pass}
               onChange={(e) => setPass(e.target.value)}
               type="password"
               placeholder="Password"
+              id="password"
+              name="password"
+            />
+          </div>
+          <div className="flex flex-col py-2">
+            <input
+              className="border p-2 rounded-2xl"
+              value={pass}
+              onChange={(e) => setPass(e.target.value)}
+              type="password"
+              placeholder="Confirm Password"
               id="password"
               name="password"
             />
