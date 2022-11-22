64e40eb903134795b959d274bf66494144131a4a
ArminAfa <90157472+Armin-AF@users.noreply.github.com>
2022-11-22 14:09:34 +0100

    Update CardComponent.js

--- client/src/components/CardComponent.js
+++ client/src/components/CardComponent.js
@@ -1,53 +1,26 @@
 import React from "react";
 
-export default function CardComponent() {
-    const posts = [
-        {
-            title: "React Tailwind Card with Grid 1",
-            img: "https://i.picsum.photos/id/192/2352/2352.jpg?hmac=jN5UExysObV7_BrOYLdxoDKzm_c_lRM6QkaInKT_1Go",
-            content: "react tailwind css card with image It is a long established fact that a reader will be distracted by the readable content"
-        },
-        {
-            title: "React Tailwind Card with Grid 2",
-            img: "https://i.picsum.photos/id/192/2352/2352.jpg?hmac=jN5UExysObV7_BrOYLdxoDKzm_c_lRM6QkaInKT_1Go",
-            content: "react tailwind css card with image It is a long established fact that a reader will be distracted by the readable content"
-        },
-        {
-            title: "React Tailwind Card with Grid 3",
-            img: "https://i.picsum.photos/id/192/2352/2352.jpg?hmac=jN5UExysObV7_BrOYLdxoDKzm_c_lRM6QkaInKT_1Go",
-            content: "react tailwind css card with image It is a long established fact that a reader will be distracted by the readable content"
-        },
-        {
-            title: "React Tailwind Card with Grid 4",
-            img: "https://i.picsum.photos/id/192/2352/2352.jpg?hmac=jN5UExysObV7_BrOYLdxoDKzm_c_lRM6QkaInKT_1Go",
-            content: "react tailwind css card with image It is a long established fact that a reader will be distracted by the readable content"
-        },
-    ];
+const CardComponent = (props) => {
     return (
-        <>
-            <div className="grid gap-2 lg:grid-cols-4">
-                {posts.map((items, key) => (
-                    <div className="w-full rounded-lg shadow-md lg:max-w-sm" key={key}>
-                        <img
-                            className="object-cover w-full h-48"
-                            src={items.img}
-                            alt="image"
-                        />
-                        <div className="p-4">
-                            <h4 className="text-xl font-semibold text-blue-600">
-
-                                {items.title}
-                            </h4>
-                            <p className="mb-2 leading-normal">
-                                {items.content}
-                            </p>
-                            <button className="px-4 py-2 text-sm text-blue-100 bg-blue-500 rounded shadow">
-                                Join
-                            </button>
-                        </div>
-                    </div>
-                ))}
+        <div className="w-full p-2 rounded-lg shadow-xl lg:flex lg:max-w-lg">
+            <img
+                className="object-cover w-full lg:w-40 lg:h-40"
+                src="https://i.picsum.photos/id/192/2352/2352.jpg?hmac=jN5UExysObV7_BrOYLdxoDKzm_c_lRM6QkaInKT_1Go"
+                alt="image"
+            />
+            <div className="pl-2">
+                <h4 className="text-xl font-semibold tracking-tight text-blue-600">
+                    Language
+                </h4>
+                <p className="mb-2 leading-normal">
+                    lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
+                <button className="px-4 py-2 text-sm text-blue-100 bg-blue-500 rounded shadow">
+                    Join
+                </button>
             </div>
-        </>
+        </div>
     );
 }
+
+export default CardComponent;
