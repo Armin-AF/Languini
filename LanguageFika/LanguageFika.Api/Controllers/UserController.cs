3f34279b96ef280f1e9f82eb2108f392e565356a
ArminAfa <90157472+Armin-AF@users.noreply.github.com>
2022-11-22 10:33:15 +0100

    UserController  with Aut0

--- LanguageFika/LanguageFika.Api/Controllers/UserController.cs
+++ LanguageFika/LanguageFika.Api/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 using LanguageFika.Api.Models;
 using LanguageFika.Api.Models.ViewModels;
 using LanguageFika.Api.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LanguageFika.Api.Controllers;
@@ -17,6 +18,7 @@ public class UserController : ControllerBase
     }
     
     [HttpGet]
+    [Authorize]
     public async Task<IActionResult> Get(){
         var users = await _userService.Users.All();
         var userViewModels = users.Select(u => u.ToViewModel());
@@ -24,12 +26,14 @@ public class UserController : ControllerBase
     }
     
     [HttpGet("{id:guid}")]
+    [Authorize]
     public async Task<IActionResult> Get(Guid id){
         var user = await _userService.Users.GetById(id);
         return Ok(user.ToViewModel());
     }
     
     [HttpPost]
+    [Authorize]
     public async Task<IActionResult> Post([FromBody] UserViewModel userViewModel){
         var user = userViewModel.ToModel();
         await _userService.Users.Add(user);
@@ -37,6 +41,7 @@ public class UserController : ControllerBase
     }
     
     [HttpPut("{id:guid}")]
+    [Authorize]
     public async Task<IActionResult> Put(Guid id, [FromBody] UserViewModel userViewModel){
         var user = await _userService.Users.GetById(id);
         userViewModel.UpdateModel(user);
@@ -45,9 +50,25 @@ public class UserController : ControllerBase
     }
     
     [HttpDelete("{id:guid}")]
+    [Authorize]
     public async Task<IActionResult> Delete(Guid id){
         var user = await _userService.Users.GetById(id);
         await _userService.Users.Delete(id);
         return NoContent();
+    }
+    
+    
+    [HttpGet("private-scoped")]
+    [Authorize("read:messages")]
+    public IActionResult Scoped() =>
+        Ok(new { Message = "Authentication and read:messages needed" });
+    
+    // This is a helper action. It allows you to easily view all the claims of the token.
+    
+    [HttpGet("claims")]
+    public IActionResult Claims(){
+        return Ok(
+            User.Claims.Select(c => new { c.Type, c.Value })
+        ); 
     }
 }
