23205b078c667a1a1ddd9ade580f7f3f1b33f965
dtimmins87 <59541874+dtimmins87@users.noreply.github.com>
2022-11-22 17:11:35 +0100

    Refactor files

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import './App.css';
 import { Landing } from "./pages/Landing";
-import { Register } from "./components/Register";
 
 function App() {
   const [currentForm, setCurrentForm] = useState('login');
@@ -15,7 +14,6 @@ function App() {
       {
         <>
         <Landing />
-        <Events />
         </>
       }
     </div>
--- client/src/pages/Events.js
+++ client/src/pages/Events.js
@@ -1,12 +1,9 @@
 import React from 'react'
-import Search from './components/Search'
-import CardList from './components/CardList'
 
 export default function Events() {
     return (
         <div>
-            <Search />
-            <CardList />
+
         </div>
     )
 }
