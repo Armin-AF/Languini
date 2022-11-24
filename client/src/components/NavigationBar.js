d7f4f8e4f3024e7847db44a865c775c8313bfa1a
dtimmins87 <59541874+dtimmins87@users.noreply.github.com>
2022-11-24 13:54:42 +0100

    update navbar

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -3,7 +3,7 @@ import LogOut from "./LogOut";
 
 const NavigationBar = () => {
     return (
-        <nav>
+        <nav className='flex justify-end gap-5 mr-5'>
             <NavLink to="/events">Events</NavLink>
             <NavLink to="/profile">Profile</NavLink>
             <NavLink to="/about">About</NavLink>
