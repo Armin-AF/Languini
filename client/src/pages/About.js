8a2d1a6fc7ffe55942a9c21b221a2877656decba
ArminAfa <90157472+Armin-AF@users.noreply.github.com>
2022-11-24 11:53:57 +0100

    Footer imp

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,7 +1,12 @@
+import Footer from "../components/Footer/Footer";
+
 const About = () => {
     return (
         <div>
             <h1>About</h1>
+            <p>Our mission is to help you find the best events in your area.</p>
+            <Footer />
+
         </div>
     );
 }
