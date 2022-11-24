fe9eaf9db28c9caf91d633628dc4aaafcffff848
ArminAfa <90157472+Armin-AF@users.noreply.github.com>
2022-11-24 16:08:44 +0100

    Update CreateEvent.js

--- client/src/pages/CreateEvent.js
+++ client/src/pages/CreateEvent.js
@@ -1,4 +1,4 @@
-import React,{useRef} from "react";
+import React, {useRef} from "react";
 
 const CreateEvent = () => {
     const inputDate = useRef(null);
@@ -7,13 +7,32 @@ const CreateEvent = () => {
     const inputLanguage = useRef(null);
     const onButtonClick = () => {
         inputDate.current.focus();
-        console.log(inputDate.current.value);
-        //inputEl.current.value.clear()
+        inputDescription.current.focus();
+        inputLocation.current.focus();
+        inputLanguage.current.focus();
+        //inputLanguage.current.value.clear()
+
+
+        const requestOptions = {
+            headers: { 'Content-Type': 'application/json' },
+            method: 'POST',
+            body: JSON.stringify({
+                date: inputDate.current.value,
+                description: inputDescription.current.value,
+                location: inputLocation.current.value,
+                language: inputLanguage.current.value
+            })
+        };
+        console.log(requestOptions)
+        fetch('https://lingofikaapi.azurewebsites.net/api/Meeting', requestOptions)
+            .then(response => {response.json()
+                console.log(response)});
     };
+
 
     return (
         <>
-            <input ref={inputDate} type="date" />
+            <input ref={inputDate} type="datetime-local" />
             <input ref={inputDescription} type="text" />
             <input ref={inputLocation} type="text" />
             <input ref={inputLanguage} type="text" />
