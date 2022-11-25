1a86dcf0ec3b41f9aa2e0cdb99e40579eec84c60
ArminAfa <90157472+Armin-AF@users.noreply.github.com>
2022-11-25 11:16:14 +0100

    Search dropdown

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import { useState } from 'react'
+import MultiSelect from "react-multiple-select-dropdown-lite";
+import "react-multiple-select-dropdown-lite/dist/index.css";
 
 const SearchBar = ({ posts, setSearchResults }) => {
 
     const [searchTerm, setSearchTerm] = useState('')
+    const [value, setvalue] = useState("");
 
     const handleSubmit = (e) => e.preventDefault()
+
+    const handleOnchange = (val) => setvalue(val);
+
+    const options = [
+        { value: "language", label: "Language" },
+        { value: "location", label: "Location" },
+        { value: "date", label: "Date" },
+    ]
 
     const handleSearchChange = (e) => {
         setSearchTerm(e.target.value)
@@ -18,11 +29,18 @@ const SearchBar = ({ posts, setSearchResults }) => {
     }
 
     return (
-        <div className="pt-2 relative mx-auto text-gray-600">
+        <div>
             <form onSubmit={handleSubmit}>
                 <input className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none mt-5"
-                    type="search" name="search" placeholder="Enter language" value={searchTerm} onChange={handleSearchChange} />
+                    type="search" name="search" placeholder="Search" value={searchTerm} onChange={handleSearchChange} />
             </form>
+            <div className={"justify-center flex mt-5"}>
+                <MultiSelect
+                    className="multi-select"
+                    onChange={handleOnchange}
+                    options={options}
+                />
+            </div>
         </div>
     )
 }
