08328351a4a5012613bb61e93b8881e54f7f08af
dtimmins87 <59541874+dtimmins87@users.noreply.github.com>
2022-11-27 13:36:19 +0100

    update card component

--- client/src/components/CardList.js
+++ client/src/components/CardList.js
@@ -4,7 +4,7 @@ const CardList = ({searchResults}) => {
     const results = searchResults.map(event => <CardComponent language={event.language} location={event.location} date={event.date} />)
     const content = results?.length ? results : <article><p>No Matching Events</p></article>
     return (
-        <main className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mt-10">{content}</main>
+        <main >{content}</main>
     )
 }
 
