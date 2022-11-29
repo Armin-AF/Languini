a392c9517d19db348f1e63eb12f9d73322b7610a
ArminAfa <90157472+Armin-AF@users.noreply.github.com>
2022-11-29 13:19:41 +0100

    Picture on navbar

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -14,12 +14,12 @@ const Profile = () => {
           <div className="my-40 w-96 h-96 pt-6 mx-auto space-y-5 flex flex-col items-center rounded overflow-hidden shadow-lg ">
               <span className="relative inline-block border-2 border-green-500 rounded-full" aria-label="avatar">
         {/* Picture */}
-                  <img src={user.picture} alt="" className="w-20 h-20 rounded-full" />
+                  <img src={user.picture} alt="" className="w-30 h-30 rounded-full" />
                   {/* Dot Status */}
                   <span className="absolute top-0 right-2 w-4 h-4 rounded-full bg-green-500 ring-2 ring-gray-50" />
                   <span className="absolute top-0 right-2 w-4 h-4 rounded-full bg-green-500 animate-ping" />
               </span>
-              <h5 className="text-lg text-gray-500 dark:text-darkgray-400">{user.given_name} {user.family_name}</h5>
+              <h5 className="text-xl text-gray-500 dark:text-darkgray-400 font-bold">{user.given_name} {user.family_name}</h5>
               <span className="text-sm text-gray-500 dark:text-darkgray-400">{user.email}</span>
               <span className="text-sm text-gray-500 dark:text-darkgray-400">Username: {user.nickname}</span>
               <span className="text-sm text-gray-500 dark:text-darkgray-400">Language: {user.locale}</span>
