c6826c4a04d3ef2cf0720d30c46f1d555c747df6
ArminAfa <90157472+Armin-AF@users.noreply.github.com>
2022-11-29 13:02:18 +0100

    Update Profile.js

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -10,16 +10,22 @@ const Profile = () => {
     isAuthenticated && (
       <>
         {isLoading ? <div><Loading/></div> :
-          
+
           <div className="my-40 w-96 h-96 pt-6 mx-auto space-y-5 flex flex-col items-center rounded overflow-hidden shadow-lg ">
-              <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src={user.picture} alt="PROFILE"/>
+              <span className="relative inline-block border-2 border-green-500 rounded-full" aria-label="avatar">
+        {/* Picture */}
+                  <img src={user.picture} alt="" className="w-20 h-20 rounded-full" />
+                  {/* Dot Status */}
+                  <span className="absolute top-0 right-2 w-4 h-4 rounded-full bg-green-500 ring-2 ring-gray-50" />
+                  <span className="absolute top-0 right-2 w-4 h-4 rounded-full bg-green-500 animate-ping" />
+              </span>
               <h5 className="text-lg text-gray-500 dark:text-darkgray-400">{user.given_name} {user.family_name}</h5>
               <span className="text-sm text-gray-500 dark:text-darkgray-400">{user.email}</span>
               <span className="text-sm text-gray-500 dark:text-darkgray-400">Username: {user.nickname}</span>
               <span className="text-sm text-gray-500 dark:text-darkgray-400">Language: {user.locale}</span>
-                            
+
           </div>}
-          
+
         <Footer/>
       </>
     )
