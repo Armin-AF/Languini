1e48642a93032162cedc64ea0ec7dff8f3e9f8fc
ArminAfa <90157472+Armin-AF@users.noreply.github.com>
2022-11-30 11:00:04 +0100

    Update ChangingText.js

--- client/src/components/ChangingText.js
+++ client/src/components/ChangingText.js
@@ -6,9 +6,9 @@ const ChangingText = () => {
         <div className="text-gray-500 text-4xl text-center font-bold uppercase flex flex-col justify-around">
             <span>Welcome to</span>
             <div className="overflow-hidden h-14">
-                <div className="animate-slide"><span className="inline-block text-white py-1 px-3 mt-1 mb-11 bg-cyan-500">Languini</span></div>
-                <div><span className="inline-block text-white py-1 px-3 mb-11 bg-green-500">Languini</span></div>
-                <div><span className="inline-block text-white py-1 px-3 mb-11 bg-red-500">Languini</span></div>
+                <div className="animate-slide"><span className="inline-block text-white py-1 px-3 mt-1 mb-11 bg-cyan-600">Languini</span></div>
+                <div><span className="inline-block text-white py-1 px-3 mb-11 bg-cyan-500">Languini</span></div>
+                <div><span className="inline-block text-white py-1 px-3 mb-11 bg-cyan-400">Languini</span></div>
             </div>
             <span></span>
         </div>
