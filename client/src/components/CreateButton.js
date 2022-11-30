bd44bed7dc2eb7b3ce54e25806ff6e9b8e61efbd
ArminAfa <90157472+Armin-AF@users.noreply.github.com>
2022-11-30 14:15:32 +0100

    Update CreateButton.js

--- client/src/components/CreateButton.js
+++ client/src/components/CreateButton.js
@@ -21,7 +21,7 @@ const Modal = (props) => {
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">
+                  <h3 className="text-3xl font-semi-bold">
                     Create Event
                   </h3>
                 </div>
@@ -32,7 +32,7 @@ const Modal = (props) => {
                 </div>
                 <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
                   <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    className="bg-red-500 hover:bg-red-400 text-white font-medium px-2 py-1 text-sm border border-gray-400 rounded shadow active:scale-[0.95] active:duration-75 transition-all h-8 w-full"
                     type="button"
                     onClick={CloseModal}
                   >
